29268be12b6f926aa89d4f301d660e831221dd0a
Jason Kroslowitz <krosljas@localhost.localdomain>
2021-01-16 15:16:48 -0600

    Optimizations

--- computerVision/findingLanes/lanes.py
+++ computerVision/findingLanes/lanes.py
@@ -4,6 +4,32 @@ import matplotlib.pyplot as plt
 
 # Edge detection technique to find lane lines in an image
 
+def make_coordinates(image, line_parameters):
+    slope, intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1*(3/5))
+    x1 = int((y1 - intercept) / slope)
+    x2 = int((y2 - intercept) / slope)
+    return np.array([x1, y1, x2, y2])
+
+def average_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+    left_line = make_coordinates(image, left_fit_average)
+    right_line = make_coordinates(image, right_fit_average)
+    return np.array([left_line, right_line])
+
 def canny(image):
     # Step 1: Convert to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
@@ -34,29 +60,45 @@ def region_of_interest(image):
 def display_lines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
-        for line in lines:
-            # Get coordinates of detected lines
-            x1, y1, x2, y2 = line.reshape(4)
+        for x1, y1, x2, y2 in lines:
             # Draw detected lines
             cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
     return line_image
 
-
+"""
 # Loading image
 image = cv2.imread('test_image.jpg')
 # Copy image
 lane_image = np.copy(image)
 
 # Calling our functions
-canny = canny(lane_image)
-cropped_image = region_of_interest(canny)
+canny_image = canny(lane_image)
+cropped_image = region_of_interest(canny_image)
 
 # Step 4: Use Hough Transform technique to detect straight lines in R.O.I.
 lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
-
+averaged_lines = average_slope_intercept(lane_image, lines)
 # Calling our function
-line_image = display_lines(lane_image, lines)
+line_image = display_lines(lane_image, averaged_lines)
 # Draw detected lines onto the original image to show lanes
 combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
 cv2.imshow('result', combo_image)
 cv2.waitKey(5000)
+"""
+
+# Make code work with video instead of an image
+cap = cv2.VideoCapture("test2.mp4")
+while(cap.isOpened()):
+    _, frame = cap.read()
+    canny_image = canny(frame)
+    cropped_image = region_of_interest(canny_image)
+    lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+    averaged_lines = average_slope_intercept(frame, lines)
+    line_image = display_lines(frame, averaged_lines)
+    combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+    cv2.imshow('result', combo_image)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
